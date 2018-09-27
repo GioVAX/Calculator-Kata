53314d226e994637d0f52d89274adc4fc9260d7c
Giovanni Pezzino <G.Pezzino@elsevier.com>
2018-09-27 23:20:52 +0100

    Refactored to use guards

--- Calculator.hs
+++ Calculator.hs
@@ -5,30 +5,28 @@ import Data.List.Split
 import Text.Regex.Posix
 
 add :: String -> Int
-add "" = 0
-add ('/':'/':'[':rest) =
-    let (_,_,s,[delims]) = ('[':rest) =~ "\\[(.*)]\n" :: (String, String, String,[String])
+add s
+    | s == "" = 0
+    | (_,_,values,[delims]) <- s =~ "//\\[(.*)]\n" :: (String, String, String,[String]) =
     -- let (_,_,s,[delims]) = ('[':rest) =~ "((?:\\[(.*)])+)\n" :: (String, String, String,[String])
-    in add' delims s (splitOn)
-add ('/':'/':rest) = 
-    let (_,_,s,[delims]) = rest =~ "(.)\n" :: (String, String, String,[String])
-    in add' ('\n':delims) s (splitOneOf)
-add s = add' "\n," s (splitOneOf)
+        add' $ splitOn delims values
+    | (_,_,values,[delims]) <- s =~ "//(.)\n" :: (String, String, String,[String]) =  
+        add' $ splitOneOf ('\n':delims) values
+    | otherwise = add' $ splitOneOf "\n," s
 
 
-add' :: String -> String -> (String -> String -> [String]) -> Int
-add' delims string splitter
+add' :: [String] -> Int
+add' strings
     | null negatives = sum $ filter (\n -> n < 1001 ) numbers
     | otherwise = raiseError negatives
     where 
-        strings = splitter delims string
         numbers = map read strings
         negatives = [n | n <- numbers, n < 0]
 
 raiseError :: [Int] -> Int
 raiseError numbers =
     let string = intercalate ", " $ map show numbers
-    in error $ (++) "negatives not allowed ==> " string
+    in error $ "negatives not allowed ==> " ++ string
 
 -- Prelude> "//,\n1,2\n3" =~ "//(.)\n" :: (String,String,String,[String])
 -- ("", "//,\n", "1,2\n3", [","])
