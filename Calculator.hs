ca071dd73b385aff7f5b30a7eb90bfa12f92e8b5
Giovanni Pezzino <g.pezzino@elsevier.com>
2018-09-28 17:44:10 +0100

    Cleanup

--- Calculator.hs
+++ Calculator.hs
@@ -1,5 +1,6 @@
 module Calculator(add) where 
 
+import Data.Char
 import Data.List
 import Data.List.Split
 import Text.Regex.PCRE
@@ -8,47 +9,45 @@ add :: String -> Int
 add input
     | input == "" = 0
     | "//[" `isPrefixOf` input = 
-        let delims = extractLongDelims "(\\[(.*?)])+?" input
+        let delims = extractLongDelims input
             values = (lines input) !! 1
-        in add' $ splitOnLongDelims delims values
-    | (_,_,values,[delims]) <- input =~ "//(.)\n" :: (String, String, String,[String]) =  
-        add' $ splitOneOf ('\n':delims) values
-    | otherwise = add' $ splitOneOf "\n," input
+        in parseAndSum $ splitOnLongDelims delims values
+    | (_,_,values,[delim]) <- input =~ "//(.)\n" :: (String, String, String,[String]) =  
+        parseAndSum $ splitOneOf ('\n':delim) values
+    | otherwise = parseAndSum $ splitOneOf "\n," input
 
-
-add' :: [String] -> Int
-add' strings
+parseAndSum :: [String] -> Int
+parseAndSum strings
     | null negatives = sum $ filter (\n -> n < 1001 ) numbers
     | otherwise = raiseError negatives
     where 
-        numbers = map read strings
-        negatives = [n | n <- numbers, n < 0]
+        -- trimmed = [(dropWhile isSpace s) | s <- strings]
+        -- negatives = [s | s <- trimmed, head s == '-']
+        -- numbers = map read trimmed
 
-raiseError :: [Int] -> Int
-raiseError numbers =
-    let string = intercalate ", " $ map show numbers
+        -- trimmed = map (\s -> dropWhile isSpace s) strings
+        -- negatives = filter (\s -> head s == '-') trimmed
+        -- numbers = map read trimmed
+
+        -- negatives = filter (\s -> head s == '-') $ map (\s -> dropWhile isSpace s) strings
+        -- numbers = map read strings
+
+        negatives = [s2 | s2 <- [(dropWhile isSpace s1) | s1 <- strings], head s2 == '-']
+        numbers = map read strings
+
+raiseError :: [String] -> Int
+raiseError negatives =
+    let string = intercalate ", " negatives
     in error $ "negatives not allowed ==> " ++ string
 
-extractLongDelims :: String -> String -> [String]
-extractLongDelims regex string =
-    map (\i -> i !! 2)  (string =~ regex :: [[String]])
-
+extractLongDelims :: String -> [String]
+extractLongDelims string =
+    map (\i -> i !! 2) (string =~ "(\\[(.*?)])+?")
 
 splitOnLongDelims :: [String] -> String -> [String]
 splitOnLongDelims delims values
-    | [delim] <- delims =  splitOn delim values
-    | otherwise = foldl (\ values delim -> concat [splitOn delim v | v <- values]) [values] delims
-
--- map (\i -> i !! 1)  ("//[***]\n1***2***3" =~ "\\[(.*?)]" :: [[String]])
--- ["***"]
-
--- map (\i -> i !! 1)  ("//[***]\n1***2***3" =~ "(\\[(.*?)])+?" :: [[String]])
--- ["[***]"]
-
-
-
--- map (\i -> i !! 2)  ("//[***]\n1***2***3" =~ "(\\[(.*?)])+?" :: [[String]])
--- ["***"]
-
--- map (\i -> i !! 2)  ("//[***][$$$]\n1***2***3" =~ "(\\[(.*?)])+?" :: [[String]])
--- ["***","$$$"]
+    | [delim] <- delims =  
+        splitOn delim values
+    | otherwise = 
+        foldl (\ values delim -> splitAndFlatten delim values) [values] delims
+        where splitAndFlatten delim values = concat [splitOn delim v | v <- values]
